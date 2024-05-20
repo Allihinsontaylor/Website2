2276de56c91b4ce47c6ba201588266894ce24169
Alliana Hinson-Taylor <142771961+Allihinsontaylor@users.noreply.github.com>
2024-05-20 09:30:52 -0700

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function changeText() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = "Hangman";
+
+    messageElement.style.backgroundColor = "white";
+
+    messageElement.style.color = "black";
+}
